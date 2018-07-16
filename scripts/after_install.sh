e605d0f421fe3ef16418c8e1f71f00c62ff7ba38
mmisbahuddin <misbah_n@hotmail.com>
2018-07-16 00:24:55 -0500

    chg

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat /usr/share/tomcat/conf/setenv.txt>>tomcat.conf
+cat /usr/share/tomcat/conf/setenv.txt>>/usr/share/tomcat/conf/tomcat.conf
 cp -R /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/code/dictionary-app /usr/share/tomcat/webapps/
 systemctl start tomcat
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+rm /usr/share/tomcat/conf/setenv.txt
 systemctl stop tomcat
+rm -rf /usr/share/tomcat/webapps/dictionary-app
+rm -rf work/Catalina/localhost/dictionary-app

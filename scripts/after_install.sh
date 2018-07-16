19396cc8804dac4c9ddea35a06e82a3826249861
mmisbahuddin <misbah_n@hotmail.com>
2018-07-16 00:55:56 -0500

    tomcat config

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rm /usr/share/tomcat/conf/tomcat.conf
+cp /usr/share/tomcat/conf/tomcat.conf.orig /usr/share/tomcat/conf/tomcat.conf
 cat /usr/share/tomcat/conf/setenv.txt>>/usr/share/tomcat/conf/tomcat.conf
 cp -R /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/code/dictionary-app /usr/share/tomcat/webapps/
 systemctl start tomcat
